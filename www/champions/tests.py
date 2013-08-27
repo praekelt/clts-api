98911b4fd51fe6b2c7bd8cef52b09db5458c3b41
Peter Pistorius <peter.pistorius@gmail.com>
2013-08-27 09:50:40 +0100

    test that activation is returning villages as well.

--- www/champions/tests.py
+++ www/champions/tests.py
@@ -1,28 +1,44 @@
 from django.test import TestCase
 from django.db import IntegrityError
+from django.utils.simplejson import loads
 
 from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from .models import Champion
+from .models import Champion, Village
 
 class ChampionApiTest(APITestCase):
 
     def setUp(self):
-        Champion.objects.create(name='Mr Dijkstra', msisdn='1234567890')
-        Champion.objects.create(name='Mr Turing', msisdn='0987654321')
+
+        self.msisdn_1 = '1234567890'
+        self.msisdn_2 = '0987654321'
+        champ = Champion.objects.create(name='Mr Dijkstra', 
+            msisdn=self.msisdn_1)
+        Champion.objects.create(name='Mr Turing', msisdn=self.msisdn_2)
+
+        Village.objects.create(champion=champ, name='South Side')
+        Village.objects.create(champion=champ, name='East Side')
 
     def test_msisdn_is_unique(self):
         with self.assertRaises(IntegrityError):
             Champion.objects.create(name='Doppel Dijkstra', 
-                                    msisdn='1234567890')
+                                    msisdn=self.msisdn_1)
 
     def test_activate_champion_via_api(self):
-        msisdn = '0987654321'
-        url = reverse('champion-activate', args=(msisdn,))
+        url = reverse('champion-activate', args=(self.msisdn_2,))
         response = self.client.post(url, None, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        c = Champion.objects.get(msisdn=msisdn)
+        c = Champion.objects.get(msisdn=self.msisdn_2)
         self.assertTrue(c.activated)
+
+    def test_activate_champions_returns_villages(self):
+        url = reverse('champion-activate', args=(self.msisdn_1,))
+        response = self.client.post(url, None, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        villages = loads(response.content)
+        self.assertEqual(len(villages), 2)
+        self.assertEqual(villages[0]['name'], 'South Side')
